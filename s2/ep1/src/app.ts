1e9e71ce634d94f66fcfcd3f75fbafe828477a94
Paul OHL <paul.lv.ohl@gmail.com>
2025-02-26 12:00:43 +0100

    wip: push to svg

---
+++ s2/ep1/src/app.ts
@@ -0,0 +1,31 @@
+interface Employe {
+	nom: string;
+	poste: string;
+}
+
+interface Client {
+	nom: string;
+	entreprise: string;
+}
+
+type PersonnePro = Employe | Client;
+
+function afficherInfoProfessionnelle(p: PersonnePro): void {
+	if ("poste" in p)
+		console.log(`Employé: ${p.nom} travaille comme ${p.poste}.`);
+	else
+		console.log(`Client: ${p.nom} est associé à l'entreprise ${p.entreprise}.`);
+}
+
+const emp: Employe = {
+	nom: "A",
+	poste: "B",
+};
+
+const cli: Client = {
+	nom: "C",
+	entreprise: "D",
+};
+
+afficherInfoProfessionnelle(emp);
+afficherInfoProfessionnelle(cli);
